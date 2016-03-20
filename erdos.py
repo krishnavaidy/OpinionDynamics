c60d17fdd9f65f3ab3f981f67286ef0978cea5b6
krishna.vaidy <krishnarules@gmail.com>
2016-03-19 22:30:36 -0400

    Changed formula, added csv file for opinions

--- erdos.py
+++ erdos.py
@@ -7,46 +7,59 @@ Krishna Vaidyanathan
 import networkx as nx
 import random
 import math
+import csv
+
+# Global variables
 
 # learning factor
 r = 1.5
 
 # Size of graph
 n = 200
-# Initialize graph
-g = nx.binomial_graph(n, 0.02)
 
-# Opinions
-# Initialize initial opinion values
-# Extreme values
-for i in range(0, 20):
-    g.node[i]['x'] = 1
+# Init graph with values
+def init_graph():
+    # Initialize graph
+    g = nx.binomial_graph(n, 0.02)
 
-for i in range(180, n):
-    g.node[i]['x'] = 0
+    # Opinions
+    # Initialize initial opinion values
+    # Extreme values
+    for i in range(0, 20):
+        g.node[i]['x'] = 1
 
-# Moderate values
-for i in range(20, 180):
-    g.node[i]['x'] = 0.5
+    for i in range(180, n):
+        g.node[i]['x'] = 0
 
-# Empathy values
-for i in range(0, n):
-    g.node[i]['h'] = random.random()%0.1
+    # Moderate values
+    for i in range(20, 180):
+        g.node[i]['x'] = random.random()%0.5
 
-# Edge weights - uniform as of now due to paucity of time
-e = g.edges()
-weights  = {}
-for i in range(0, len(e)):
-    weights[e[i]] = 1
+    # Empathy vanlues
+    for i in range(0, n):
+        g.node[i]['h'] = random.random()%0.01
 
-nx.set_edge_attributes(g, 'w', weights)
-for i in g.nodes():
-    #g[g.node[i]][g.node[i]]['w'] = 1
-    g.add_edge(i,i)
-    g[i][i]['w'] = 1
+    # Edge weights - uniform as of now due to paucity of time
+    e = g.edges()
+    weights  = {}
+    for i in range(0, len(e)):
+        weights[e[i]] = 1
 
-# Rounds
-def opinion_update():
+    nx.set_edge_attributes(g, 'w', weights)
+    for i in g.nodes():
+        #g[g.node[i]][g.node[i]]['w'] = 1
+        g.add_edge(i,i)
+        e = nx.all_neighbors(g, i)
+        l = 0
+        for _ in e:
+            l = l + 1
+
+        g[i][i]['w'] = l
+
+    return g
+
+# Updates opinions and weights of graph g
+def opinion_update(g):
 
     # Update opinions
     for i in g.nodes():
@@ -58,17 +71,38 @@ def opinion_update():
             weighted_opinion = weighted_opinion + g[i][j]['w']* g.node[j]['x']
             weights = weights + g[i][j]['w']
 
-        g.node[i]['x'] = (g.node[i]['x'] * g[i][i]['w'] + weighted_opinion)/(g[i][i]['w'] + weighted_opinion)
+        g.node[i]['x'] = (g.node[i]['x'] * g[i][i]['w'] + weighted_opinion)/(g[i][i]['w'] + weights)
 
     # Update weights
     e = g.edges()
     for i in e:
         exp_value = math.pow((g.node[i[0]]['x'] - g.node[i[1]]['x']),2)
-        T = math.exp(exp_value/g.node[i[0]]['h'])
-        g[i[0]][i[1]]['w'] = (g[i[0]][i[0]]['w'] + r*T)/(1 + r)
+        T = math.exp(-exp_value/g.node[i[0]]['h'])
+        g[i[0]][i[1]]['w'] = (g[i[0]][i[1]]['w'] + r*T)/(1 + r)
 
 
 
-opinion_update()
+if __name__ == "__main__":
+    # Number of iterations to avg opinion value over
+    iterations = 25
+    # Number of rounds for each iteration
+    rounds = 500
+    x_list = [0] * n
+    h_list = [0] * n
+    for i in range(0, iterations):
+        g = init_graph()
+        for j in range(0, rounds):
+            opinion_update(g)
+        nodes = g.nodes()
+        for xi in nodes:
+            x_list[xi] = x_list[xi] + g.node[xi]['x']
+            h_list[xi] = h_list[xi] + g.node[xi]['h']
 
-
+    x_list[:] = [x / (iterations) for x in x_list]
+    h_list[:] = [h / (iterations) for h in h_list]
+    # write to csv file
+    wr_x = csv.writer(open("results.csv", 'wb'), dialect='excel')
+    wr_h = csv.writer(open("h.csv", 'wv'), dialect='excel')
+    for (i, j) in zip(x_list, h_list):
+        wr_x.writerow([i])
+        wr_h.writerow([j])
